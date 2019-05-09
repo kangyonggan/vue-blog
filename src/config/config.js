733dccfed4cbfa3dc028701418d799b6fa9fd76a
kangyonggan <kangyonggan@gmail.com>
2019-05-09 14:40:22 +0800

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,6 @@ import Env from './env';
 
 const baseUrl = Env === 'development' ? 'http://localhost:8080' : (Env === 'mock' ? '' : 'https://kangyonggan.com/web');
 
-console.log(baseUrl);
-
 let config = {
     env: Env,
     baseUrl: baseUrl
