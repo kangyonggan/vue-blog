d6b46360aa9546fc2d6ccd29383ebd93637747c3
kangyonggan <kangyonggan@gmail.com>
2019-05-31 17:49:11 +0800

    search

--- src/components/app-search.vue
+++ src/components/app-search.vue
@@ -12,18 +12,20 @@
                 <div class="input" :class="{preResult: showTips}">
                     <Icon type="ios-search" size="28" color="#bdc4d5"/>
                     <input name="key" v-model="key" autocomplete='off' :placeholder="placeholder" @keyup.enter="search"
-                           @input="preSearch"/>
+                           @input="preSearch" @keydown="keydown"/>
                     <div class="input-append">
                         <span>|</span>
                         <a href="javascript:" @click="search">搜 索</a>
                     </div>
                     <div class="tips" v-show="showTips">
                         <ul v-if="currIndex===0">
-                            <li v-for="item in preList" @click="selectPreItem($event, item)">{{item}}</li>
+                            <li :class="{active: cursorIndex===index}" v-for="(item, index) in preList"
+                                :key="index" @click="selectPreItem($event, item)">{{item}}</li>
                             <AppLoading :loading="isSearching" :height="150"/>
                         </ul>
                         <ul v-if="currIndex===1">
-                            <li v-for="item in preList" @click="selectPreItem($event, item.name)">
+                            <li :class="{active: cursorIndex===index}" v-for="(item, index) in preList"
+                                :key="index" @click="selectPreItem($event, item.name)">
                                 <span>{{item.name}}</span>
                                 <span class="pre-author">{{item.author}}</span>
                             </li>
@@ -68,7 +70,8 @@
                 key: '',
                 preList: [],
                 isSearching: false,
-                showTips: false
+                showTips: false,
+                cursorIndex: -1
             };
         },
         mounted: function () {
@@ -89,6 +92,14 @@
                 key = '';
             }
             this.key = key;
+
+            let that = this;
+            // 点击空白区域关闭
+            document.onclick = function (e) {
+                if (e.target.getAttribute('name') !== 'key') {
+                    that.showTips = false;
+                }
+            }
         },
         watch: {
             '$route'() {
@@ -156,6 +167,7 @@
                 if (e.target.value) {
                     this.showTips = true;
                     this.isSearching = true;
+                    this.cursorIndex = -1;
                     if (this.currIndex === 0) {
                         // 预搜索文章
                         this.http.post('/article/preSearch', {key: e.target.value}).then(res => {
@@ -186,6 +198,33 @@
                     this.key = val;
                 }
                 this.showTips = false;
+            },
+            keydown: function (e) {
+                if (this.showTips) {
+                    if (e.keyCode === 38) {
+                        // 上
+                        this.cursorIndex = this.cursorIndex - 1;
+                        if (this.cursorIndex < 0) {
+                            this.cursorIndex = this.preList.length - 1;
+                        }
+                    } else if (e.keyCode === 40) {
+                        // 下
+                        this.cursorIndex = this.cursorIndex + 1;
+                        if (this.cursorIndex >= this.preList.length) {
+                            this.cursorIndex = 0;
+                        }
+                    }
+
+                    if (this.cursorIndex >= 0) {
+                        if (this.currIndex === 0) {
+                            this.key = this.preList[this.cursorIndex];
+                        } else {
+                            this.key = this.preList[this.cursorIndex].name;
+                        }
+                    }
+                }
+
+                return true;
             }
         }
     };
@@ -288,6 +327,10 @@
                 }
 
                 li:hover {
+                    background: #f5f5f5;
+                }
+
+                li.active {
                     background: #f5f5f5;
                     color: @primary-color;
                 }
